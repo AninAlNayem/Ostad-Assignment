9725fa22cd5d04d31f4c7aee56a180fc4b8eab78
Nayem1503019 <106160241+Nayem1503019@users.noreply.github.com>
2023-12-18 23:10:02 +0600

    Create Task 1: Shuffle String.cpp

---
+++ Week-3/Task 1: Shuffle String.cpp
@@ -0,0 +1,15 @@
+string restoreString(string s, vector<int>& indices) {
+        char str[105];
+        int n=indices.size();
+        for(int i=0;i<n;i++){
+            int idx=indices[i];
+            str[idx]=s[i];
+        }
+        string ans="";
+        for(int i=0;i<n;i++){
+           ans.push_back(str[i]);
+        }
+       return ans;
+    }
+
+// Time Complexity: O(n) and Space Complexity: O(n)
