fbba78562a11b42ce04fad492d2556b2e34c572a
Nayem1503019 <106160241+Nayem1503019@users.noreply.github.com>
2023-12-25 21:40:54 +0600

    Create Task-2: Multiplication.cpp

---
+++ Week-4/Task-2: Multiplication.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+ll solve(ll n,ll m){
+    if(n==1){
+      return m;
+    }
+    return m+solve(n-1,m);
+}
+
+int main() {
+  	ll n,m;cin>>n>>m;
+  	cout<<solve(n,m);
+return 0;
+}
